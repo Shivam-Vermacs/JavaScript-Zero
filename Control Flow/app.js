9927ff270d25cf354482fbac5f31a602579264ca
Shivam Verma <161146158+Shivam-Vermacs@users.noreply.github.com>
2025-02-23 19:44:19 +0530

    Add files via upload

---
+++ Control Flow/app.js
@@ -0,0 +1,119 @@
+// Conditional Statements
+
+// let time = 10;
+
+// if (time === 10) {
+//   console.log("it is 10");
+// } else {
+//   console.log("it isnt 10 it must be something bish");
+// }
+
+// challenge1
+
+// let password;
+
+// if (password === 8) {
+//   console.log("Welcome");
+// } else if (password <= 8) {
+//   console.log("Password is too short");
+// } else if (password >= 8) {
+//   console.log("Password is too long");
+// } else {
+//   console.log("Please Provide a Password");
+// }
+
+// Switch Statements in Js
+
+// let me = 0;
+// let final;
+
+// switch (me) {
+//   case 0:
+//     final = "success";
+//     console.log(final);
+//     break;
+
+//   case 1:
+//     final = "failure";
+//     console.log(final);
+//     break;
+
+//   default:
+//     final = "no value found";
+//     console.log(final);
+// }
+
+// challenge2
+
+// let fruit = "Banana";
+
+// switch (fruit) {
+//   case "Banana":
+//     console.log("Banana is good!");
+//     break;
+//   case "Orange":
+//     console.log("Not a fan of orange");
+//     break;
+//   case "Apple":
+//     console.log("Apple a day keeps me away");
+//     break;
+
+//   default:
+//     console.log("I have never heard of that fruit");
+//     break;
+// }
+
+// For Loop
+
+// Challenge ( No and Name from 0 to 1000)
+
+// for (let i = 0; i <= 1000; i++) {
+//   console.log("Lee Joo Young ", i);
+// }
+
+// While Loop
+
+// Challlenge (Print your Name 100 times starting from 10)
+// let i = 10;
+// while (i <= 100) {
+//   console.log("Joo young Noona");
+//   i++;
+// }
+
+// Do while loop
+
+// let i = 20;
+
+// do {
+//   console.log("noona", i);
+//   i++;
+// } while (i <= 400);
+
+// Logical Operators
+// (&&) Logical AND and (||)Logical OR and (!)Logical NOT
+
+// Arrays
+// let arr = ["park jin young", "park seo joon", "park hyung sik"];
+// console.log(arr);
+
+// let noArr = [1, 2, 3, 5, 6, 7];
+// console.log(noArr);
+
+// accessing items from our array
+
+// let arrNum = [1, 2, 3, 4, 5, 6];
+// console.log(arrNum[3]);
+
+// Nested Array || 2D Array
+// let arrayNum = [
+//   [1, 2],
+//   [2, 3],
+// ];
+// console.log(arrayNum[0]);
+
+//Challenge
+// let favSingers = ["Ben", "Venesa", "lilboi"];
+// console.log(favSingers[0]);
+// let favNumbers = [7, 8, 1, 0];
+// let mixedArr = ["string", [favNumbers], 123, true];
+// console.log(mixedArr[0], mixedArr[1], mixedArr[2], mixedArr[3]);
