f064963cabc4322b50188912aa8bb7176f2184c0
Shivam Verma <161146158+Shivam-Vermacs@users.noreply.github.com>
2025-02-28 04:45:33 +0530

    Add files via upload

---
+++ Objects & JSON/app.js
@@ -0,0 +1,50 @@
+//Methods
+//How to create a method and pass the reference of that method
+
+//1st way
+
+// function sorry() {
+//     return `I'm sorry for letting you down ${mubad.ame} and ill never repeat such a ${mubad.thing}`;
+// }
+
+// const mubad = {
+//     ame: "Bro",
+//     thing: 23,
+//     sorry,
+
+// };
+
+// console.log(mubad.sorry());
+
+// //Second Way
+// const mb = {
+//     named: "Sis",
+//     ager: 49,
+//     ifku: function fck() {
+//         return `i think i need to lock tf in , ${mb.named}`;
+        
+//     },
+// };
+
+// console.log(mb.ifku());
+
+
+
+//JSON(javascript Object Notation)
+
+// const jsn = {
+//     name:"Shivam",
+//     age: 12,
+//     gender: "male",
+//     city: "lucknow",
+//     state: "uttar pradesh",
+//     college:"SRM"
+    
+// }
+
+// const jjds = JSON.stringify(jsn);
+// const parsed = JSON.parse(jjds);
+// console.log(parsed);
+
+
+
