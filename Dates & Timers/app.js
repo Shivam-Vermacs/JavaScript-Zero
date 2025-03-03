bebfab4566f40f6ffb1b5f8a985df977e2552998
Shivam Verma <161146158+Shivam-Vermacs@users.noreply.github.com>
2025-03-04 01:28:50 +0530

    Add files via upload

---
+++ Dates & Timers/app.js
@@ -0,0 +1,76 @@
+//Dates
+
+// const datee = new Date()
+// console.log(datee);
+
+
+// //another Date (year , month , day , hours , minutes , seconds , milliseconds)
+// const currentDate = new Date();
+// console.log(currentDate);
+
+
+//  const yearCurrent = datee.getFullYear();
+//  const monthCurrent = datee.getMonth();
+//  const  dayCurrent = datee.getDay();
+//  const hourCurrent = datee.getHours();
+//  const minutesCurrent = datee.getMinutes();
+//  const secondsCurrent = datee.getSeconds();
+ 
+
+// // console.log(`Year:${yearCurrent}`);
+// console.log(`Month:${monthCurrent}`);
+// console.log(`Day:${dayCurrent}`);
+// console.log(`Hours:${hourCurrent}`);
+// console.log(`Minutes:${minutesCurrent}`);
+// console.log(`Seconds:${secondsCurrent}`);
+// console.log(datee.toISOString());
+// console.log(datee.toLocaleString());
+
+
+
+//Set Interval -> Executes a function repeatedly at a fixed time interval.
+// setInterval(() =>
+//     console.log(`this finction is funciton 10`)
+//     , 1000);
+
+//Set Timeout -> Executes a function once after a specified delay (in milliseconds).
+// setTimeout(function () {
+//     console.log('this function will execute after 3 seconds');
+// }, 1000);
+
+//clear Interval 
+// stops a setInterval() from repeating.
+
+// let intervalID = setInterval(function () {
+//     console.log('this function will stop after 10 sec');
+// }, 1000);
+
+// setTimeout(function () {
+//     clearInterval(intervalID);
+//     console.log('interval Stopped');
+// }, 10000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Set Timeout
